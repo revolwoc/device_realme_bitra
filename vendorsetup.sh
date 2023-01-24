d82afb0d5c50c99e74d91b612bf193442f469b54
Blackmanx <blackman@sajl.tk>
2023-01-24 14:53:35 +0100

    bitra: vendorsetup: Add camera aux unlock script
    Apparently some roms that aren't based on LineageOS don't like or are missing the new camera shim, so I'll add this for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,4 +7,14 @@ else
 	git fetch https://review.arrowos.net/ArrowOS/android_frameworks_base refs/changes/88/17688/1 && git cherry-pick FETCH_HEAD
 fi
 
+cd ..
+
+cd av || exit 1
+
+if git log -150 --oneline --decorate | grep "Expose aux cameras in third party apps"; then
+	echo "Fix was already applied"
+else
+	git fetch https://github.com/Project-Elixir/frameworks_av/ c1da308642fc5bed9aa98bf07ca3d7a2beec717d && git cherry-pick FETCH_HEAD
+fi
+
 cd ../..
